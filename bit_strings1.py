6f2c45d55a4605d0fbd2ff2585c8142233f807f6
isabellagomez <ilgomezt@ucsd.edu>
2020-10-24 20:30:48 -0700

    TRY 1 bit_strings1.py. Checks if the dict has appropriate length, but not that each entry is perfect

--- bit_strings1.py
+++ bit_strings1.py
@@ -8,16 +8,39 @@
 # bits. The values of the dictionary correspond of the probability
 # of drawing any one of these.
 
+import random
 def get_sample(nbits=3,prob=None,n=1):
     '''
-    This function
+    This function takes in a dictionary of bits and probabilities and
+    returns an n number of random samples from the keys of the dict.
+    The size of the dictionary is dependent on the number nbits of
+    digits given.
 
-    :param nbits:
-    :param prob:
-    :param n:
-    :return:
+    :param nbits: the number of bits in the dictionary keys
+    :param prob: dictionary of bits and probabilities
+    :param n: length of return list
+    :return: random_entry_list containing the n random samples taken
     '''
 
-    n = []
-    return n
+    # check that n and nbits are ints
+    assert type(nbits) == int
+    assert type(n) == int
+
+    # check that prop is a dict if not NONE
+    if prob != None:
+        assert type(prob) == dict
+        # check that the dictionary has the appropriate num of entries
+        assert len(prob.keys()) == 2 ** nbits
+    else:
+        return
+
+    total_entry_list = list(prob.items()) # contains all the dict entries as tuples
+    random_entry_list = [] # will hold n number of entries from dict
+
+    # will add the n random samples to the list
+    for i in range(n):
+        random_tuple = random.choice(total_entry_list)
+        random_entry_list.append(random_tuple[0])
+
+    return random_entry_list
 
